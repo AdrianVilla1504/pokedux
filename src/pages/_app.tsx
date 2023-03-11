0a11e362223226fe2165c17b57672fe7d70a5e01
Adrian Villa <adriancvilla@gmail.com>
2023-03-11 11:41:01 -0500

    feat: add devtools to track changes on store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
+import {
+  composeWithDevTools,
+  devToolsEnhancer,
+} from "@redux-devtools/extension";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const store = createStore(pokemonsReducer);
+  const store = createStore(pokemonsReducer, composeWithDevTools());
 
   return (
     <>
